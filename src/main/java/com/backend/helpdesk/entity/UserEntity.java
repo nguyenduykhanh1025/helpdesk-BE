b5254ce8473095e1c3f98acceb84ea746002e56d
unknown <kunlezkunlez1025@gmail.com>
2019-09-11 23:06:38 +0700

    create relationship user and role

--- src/main/java/com/backend/helpdesk/entity/UserEntity.java
+++ src/main/java/com/backend/helpdesk/entity/UserEntity.java
@@ -2,6 +2,7 @@ package com.backend.helpdesk.entity;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Set;
 
 @Entity(name="users")
 public class UserEntity {
@@ -36,6 +37,14 @@ public class UserEntity {
 
     @Column(nullable = false)
     private Date startingDay;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<RoleEntity> roleEntities;
 
     public UserEntity() {
     }
