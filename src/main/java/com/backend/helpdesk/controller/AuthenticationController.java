7a2c502c124feea0fc3117cd97f109ee4b49f0f7
unknown <kunlezkunlez1025@gmail.com>
2019-09-12 00:33:52 +0700

    finish  AuthenticationController

--- src/main/java/com/backend/helpdesk/controller/AuthenticationController.java
+++ src/main/java/com/backend/helpdesk/controller/AuthenticationController.java
@@ -6,9 +6,6 @@ import com.backend.helpdesk.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
