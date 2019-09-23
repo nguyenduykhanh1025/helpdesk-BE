13d0bd7d5edf942d7f7bea6f2bf9c71341f80b6e
nhi <nguyenthinhi0602@gmail.com>
2019-09-23 12:53:43 +0700

    edit get list of my day offs this year and get number of day off used

--- src/main/java/com/backend/helpdesk/common/CommonMethods.java
+++ src/main/java/com/backend/helpdesk/common/CommonMethods.java
@@ -1,5 +1,7 @@
 package com.backend.helpdesk.common;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.regex.Pattern;
 
 public class CommonMethods {
@@ -8,5 +10,25 @@ public class CommonMethods {
         String query = "[a-z][a-z0-9_\\.]{5,32}@novahub.vn";
 
         return Pattern.matches(query, email);
+    }
+    public static float calculateDaysBetweenTwoDate(Date date1, Date date2) {
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(date1);
+        cal2.setTime(date2);
+        float numberOfDays = 0;
+        while (cal1.before(cal2)) {
+            if ((Calendar.SATURDAY != cal1.get(Calendar.DAY_OF_WEEK))
+                    && (Calendar.SUNDAY != cal1.get(Calendar.DAY_OF_WEEK))) {
+                numberOfDays++;
+            }
+            cal1.add(Calendar.DATE, 1);
+        }
+        if (date1.getHours() == 12 && date2.getHours() == 12) {
+            numberOfDays--;
+        } else if (date1.getHours() == 8 && date2.getHours() == 12 || date1.getHours() == 12 && date2.getHours() == 18) {
+            numberOfDays = numberOfDays - 0.5f;
+        }
+        return numberOfDays;
     }
 }
--- src/main/java/com/backend/helpdesk/service/DayOffService.java
+++ src/main/java/com/backend/helpdesk/service/DayOffService.java
@@ -1,5 +1,6 @@
 package com.backend.helpdesk.service;
 
+import com.backend.helpdesk.common.CommonMethods;
 import com.backend.helpdesk.common.Constants;
 import com.backend.helpdesk.converter.Converter;
 import com.backend.helpdesk.converter.DayOffToDayOffDTOConverter;
@@ -83,26 +84,7 @@ public class DayOffService {
         return ChronoUnit.YEARS.between(startingLocalDate, now) + Constants.DAYOFFBYRULE;
     }
 
-    public float calculateDaysBetweenTwoDate(Date date1, Date date2) {
-        Calendar cal1 = Calendar.getInstance();
-        Calendar cal2 = Calendar.getInstance();
-        cal1.setTime(date1);
-        cal2.setTime(date2);
-        float numberOfDays = 0;
-        while (cal1.before(cal2)) {
-            if ((Calendar.SATURDAY != cal1.get(Calendar.DAY_OF_WEEK))
-                    && (Calendar.SUNDAY != cal1.get(Calendar.DAY_OF_WEEK))) {
-                numberOfDays++;
-            }
-            cal1.add(Calendar.DATE, 1);
-        }
-        if (date1.getHours() == 12 && date2.getHours() == 12) {
-            numberOfDays--;
-        } else if (date1.getHours() == 8 && date2.getHours() == 12 || date1.getHours() == 12 && date2.getHours() == 18) {
-            numberOfDays = numberOfDays - 0.5f;
-        }
-        return numberOfDays;
-    }
+
 
     public float getNumberOfDayOffUsed(int id, int year) {
         UserEntity userEntity = userRepository.findById(id);
@@ -112,7 +94,7 @@ public class DayOffService {
         List<DayOff> dayOffs = dayOffRepository.getDayOffByYear(year, id);
         float result = 0;
         for (DayOff dayOff : dayOffs) {
-            result += calculateDaysBetweenTwoDate(dayOff.getDayStartOff(), dayOff.getDayEndOff());
+            result += CommonMethods.calculateDaysBetweenTwoDate(dayOff.getDayStartOff(), dayOff.getDayEndOff());
         }
         int a = 0;
         return result;
@@ -126,4 +108,5 @@ public class DayOffService {
         List<DayOff> dayOffs = dayOffRepository.getDayOffByYear(year, id);
         return dayOffDayOffDTOConverter.convert(dayOffs);
     }
+
 }
