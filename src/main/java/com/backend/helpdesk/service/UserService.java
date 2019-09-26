483b594979e90a3641b9118b49a649e66bf824f2
unknown <kunlezkunlez1025@gmail.com>
2019-09-26 20:17:20 +0700

    format code

--- src/main/java/com/backend/helpdesk/service/UserService.java
+++ src/main/java/com/backend/helpdesk/service/UserService.java
@@ -14,8 +14,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-import static com.backend.helpdesk.common.Constants.*;
-
 @Service
 public class UserService {
 
